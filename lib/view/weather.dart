7db6b9b550d4f039a4306ab6a5e6255817483be8
AdlilAfiq11 <adlilafiq11@gmail.com>
2024-01-12 17:59:18 +0800

    Create weather page

---
+++ lib/view/weather.dart
@@ -0,0 +1,279 @@
+import 'package:flutter/material.dart';
+
+class WeatherPageView extends StatefulWidget {
+  const WeatherPageView({super.key});
+
+  @override
+  State<WeatherPageView> createState() => _WeatherPageViewState();
+}
+
+class _WeatherPageViewState extends State<WeatherPageView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.black,
+            Colors.blue.shade900,
+            Colors.purple.shade600,
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              "assets/images/weatherIcon.png",
+              scale: 3.0,
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+              child: Text(
+                '28°C',
+                style: TextStyle(
+                  fontSize: 60,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+              child: Text(
+                'Precipitations',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 4, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Max: 24°',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    'Max: 18°',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 40, 0, 30),
+              child: GestureDetector(
+                onTap: () => showTodayWeather(),
+                child: Image.asset(
+                  "assets/images/house.png",
+                  scale: 2.6,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  showTodayWeather() => showModalBottomSheet(
+      backgroundColor: Colors.blue.shade900,
+      showDragHandle: true,
+      elevation: 3.0,
+      context: context,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(30))),
+      builder: (context) {
+        return Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.blue.shade900,
+                Colors.purple.shade600,
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          height: 350,
+          child: const Column(
+            children: <Widget>[
+              ListTile(
+                leading: Text('Today',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    )),
+                trailing: Text('February, 10',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    )),
+              ),
+              Divider(
+                thickness: 2.0,
+                indent: 20.0,
+                endIndent: 20.0,
+                height: 0.1,
+                color: Colors.white24,
+              ),
+              ListTile(
+                title: Text('Weather Forecasts',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    )),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 15, 0, 10),
+                        child: Text(
+                          '28°C',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Image(
+                        image: AssetImage("assets/images/weatherIcon.png"),
+                        width: 70,
+                        height: 70,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                        child: Text(
+                          '15.00',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 15, 0, 10),
+                        child: Text(
+                          '28°C',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Image(
+                        image: AssetImage("assets/images/weatherIcon.png"),
+                        width: 70,
+                        height: 70,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                        child: Text(
+                          '16.00',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 15, 0, 10),
+                        child: Text(
+                          '28°C',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Image(
+                        image: AssetImage("assets/images/weatherIcon.png"),
+                        width: 70,
+                        height: 70,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                        child: Text(
+                          '17.00',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 15, 0, 10),
+                        child: Text(
+                          '28°C',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Image(
+                        image: AssetImage("assets/images/weatherIcon.png"),
+                        width: 70,
+                        height: 70,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                        child: Text(
+                          '18.00',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ListTile(
+                title: Text(
+                    '*Disclaimer: The weather forecasts is just a prediction, not totally accurate.',
+                    style: TextStyle(
+                      fontSize: 11,
+                      color: Colors.white,
+                    )),
+              ),
+            ],
+          ),
+        );
+      });
+}
